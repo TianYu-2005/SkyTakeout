24feaec626a84da35809ec4d0348d070f2dcc572
231870242 <231870242@smail.nju.edu.cn>
2025-11-11 17:10:12 +0800

    完成阿里云OSS部分

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,12 @@
+package com.sky.controller.admin;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin/dish")
+@Slf4j
+@Api(tags = "菜品相关接口")
+public class DishController {
+
+}
